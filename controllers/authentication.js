f54c75cc196d25c2c9092c4f034e96b1ed5311dd
kentah <kenthow@gmail.com>
2016-05-07 13:15:51 -0500

    Final commit, working auth

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,6 +8,12 @@ function tokenForUser(user){
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req,res,next){
+    // User has already had their email authed
+    // we just need to give them a tokenForUser
+    res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req,res,next){
 
   // See if a user with the given email exists
